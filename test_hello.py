4980de7a664bd833c76e3400682a956eeb700afb
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-09-25 02:13:33 +0000

    hello test

---
+++ test_hello.py
@@ -0,0 +1,14 @@
+def greetings():
+
+    return print('hello, minions')
+
+
+
+
+def main():
+    greetings()
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    main()
