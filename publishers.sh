ee670a867be34ec8011f8d50e2a4f515490ca184
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-10-11 23:49:56 +0000

    publisher has a top 25 category added

--- publishers.sh
+++ publishers.sh
@@ -11,9 +11,9 @@ read -p "Enter the name of your favorite publisher: " publisher
 
 
 head -n 1 $path | cut -d ',' -f 1-5,10 > your_publisher.csv
-cat $path | cut -d ',' -f 1-5,10 | grep -n "$publisher" | sort >> your_publisher.csv
+cat $path | cut -d ',' -f 1-5,10 | grep -n "$publisher" | sort -t , -nr -k 6 >> your_publisher.csv
 
 echo "Count of titles under $publisher"
 cat your_publisher.csv | wc -l
-echo "Now printing the first 25 titles under $publisher"
+echo "Now printing the best selling 25 titles under $publisher"
 head -n 26 your_publisher.csv
